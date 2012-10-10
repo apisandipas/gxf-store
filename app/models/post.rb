913451283410153120dc1f35a31d020ff79f0acd
Bryan Paronto <bparonto@gmail.com>
2012-10-09 22:55:41 -0700

    fixed default scope of posts to DESC by date

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :published, :title
 
+  default_scope :order => 'created_at DESC'
 
   before_create do |post|
     post.slug = post.title.slugify
